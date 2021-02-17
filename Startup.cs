5391a0cc28610dadcd15d7e9c6b0a7e5a7b8aa04
johndavid25 <75648891+johndavid25@users.noreply.github.com>
2021-02-17 14:10:12 -0500

    DataUtility Changes

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using BugTracker.Data;
 using BugTracker.Models;
+using BugTracker.Utilities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -23,8 +24,7 @@ namespace BugTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(DataUtility.GetConnectionString(Configuration)));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddIdentity<BTUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
---
+++ Utilities/DataUtility.cs
@@ -0,0 +1,50 @@
+﻿using BugTracker.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace BugTracker.Utilities
+{
+    public class DataUtility
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+
+        public static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(":");
+
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true,
+            };
+            return builder.ToString();
+        }
+
+        public static async Task ManageDataAsync(IHost host)
+        {
+            using var svcScope = host.Services.CreateScope();
+            var svcProvider = svcScope.ServiceProvider;
+
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            await dbContextSvc.Database.MigrateAsync();          
+        }
+    }
+}
